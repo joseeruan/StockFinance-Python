4081986b18ea7ae61c9816897b2eb141b099e5b0
José Ruan <143905670+joseeruan@users.noreply.github.com>
2023-12-22 00:27:49 +0000

    Update

---
+++ data/download.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import pandas as pd
+
+def download_data (ticker:str) -> pd.DataFrame:
+
+    """Download data from Yahoo Finances
+
+    Args:
+        ticker (str): the ticker of financial asset.
+
+    Returns:
+        pd.DataFrame: A datafram retrieved from Yahoo Finances.
+    """
+
+    data = yf.download(ticker)
+
+    return data
+
